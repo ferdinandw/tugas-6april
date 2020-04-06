0d399198cbe5823b070eb1dc47cbec5cc339abf8
ferdinandw <fwng123@gmail.com>
2020-04-06 15:33:47 +0700

    tugas

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const myContacts = [
-    {
+const myContacts = [{
         id: 1,
         name: 'Alpha Avalon',
         phone: '+1 111 101010',
@@ -20,3 +19,51 @@ const myContacts = [
     }
 
 ]
+
+const showContact = (myContacts) => {
+    for (let index = 0; index < myContacts.length; index++) {
+        const contact = myContacts[index];
+        console.log(`${contact.id}, ${contact.name}`)
+    }
+}
+console.log('data awal')
+showContact(myContacts)
+
+const ferdi = {
+    id: 4,
+    name: 'Ferdi',
+    phone: '+62 813 7803',
+    email: 'ferdi@gmail.com'
+}
+
+const addContact = () => {
+    myContacts.push(ferdi);
+    showContact(myContacts)
+}
+
+const deleteId = () => {
+    myContacts.pop();
+    showContact(myContacts)
+}
+
+const filter1 = myContacts.filter((myContacts) => {
+    if (myContacts.id == "1")
+        return true;
+    return false;
+})
+const filter2 = myContacts.filter((myContacts) => {
+    if (myContacts.id == "2")
+        return true;
+    return false;
+})
+const filter3 = myContacts.filter((myContacts) => {
+    if (myContacts.id == "3")
+        return true;
+    return false;
+})
+
+const testFilter = (myContacts) =>{
+    if(myContacts.id =="1")
+    return true;
+    return false;
+}
